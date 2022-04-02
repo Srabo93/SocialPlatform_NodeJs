8ba399e2d4290c2005455aa17bddf22acc13057c
Srabo93 <A.S-dev@web.de>
2022-04-02 15:13:47 +0200

    added oauth for google, needed middleware, passport

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require("express");
+const path = require("path");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const morgan = require("morgan");
 const { engine } = require("express-handlebars");
+const session = require("express-session");
+const passport = require("passport");
 /*Load Config */
 dotenv.config({ path: "./config/config.env" });
+/*Passport Config */
+require("./config/passport")(passport);
 /*Connect DB */
 connectDB();
 /*Initialize App */
@@ -16,8 +21,21 @@ if (process.env.NODE_ENV === "development") {
 /*Handlebars */
 app.engine(".hbs", engine({ extname: ".hbs" }));
 app.set("view engine", ".hbs");
-app.set("views", "./views");
-
+/*Sessions */
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+/*Passport Middleware */
+app.use(passport.initialize());
+app.use(passport.session());
+/*Routes */
+app.use("/", require("./routes/index"));
+/*Define Static Folder */
+app.use(express.static(path.join(__dirname, "public")));
 /*Define Ports */
 const PORT = process.env.PORT || 5000;
 
