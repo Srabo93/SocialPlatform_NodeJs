329e202e2659d3a22e3057eda3568e278e6640f8
Srabo93 <A.S-dev@web.de>
2022-04-02 11:45:12 +0200

    added template enginge handlebars

--- app.js
+++ app.js
@@ -2,15 +2,22 @@ const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const morgan = require("morgan");
+const { engine } = require("express-handlebars");
 /*Load Config */
 dotenv.config({ path: "./config/config.env" });
 /*Connect DB */
 connectDB();
 /*Initialize App */
 const app = express();
+/*Logging Devenviroment */
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+/*Handlebars */
+app.engine(".hbs", engine({ extname: ".hbs" }));
+app.set("view engine", ".hbs");
+app.set("views", "./views");
+
 /*Define Ports */
 const PORT = process.env.PORT || 5000;
 
