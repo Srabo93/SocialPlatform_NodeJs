c993695553de8491e53acc0306566abeab488d5f
Srabo93 <a.s-dev@web.de>
2023-03-01 22:49:25 +0100

    adjusted build script

--- copyStaticAssets.js
+++ copyStaticAssets.js
@@ -1,28 +1,15 @@
-"use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-// import * as shell from "shelljs";
-const shell = __importStar(require("shelljs"));
-shell.cp("-R", "src/public/", "build/public/");
+const shell = require("shelljs");
+const fs = require("fs");
+
+// directory to check if exists
+const dir = "./build/public/";
+
+// check if directory exists
+if (fs.existsSync(dir)) {
+  console.log("Public Directory exists, will be deleted and rebuild");
+  shell.rm("-rf", dir);
+  shell.cp("-R", "src/public/", "build/public/");
+} else {
+  console.log("Public Directory not found, is getting created!");
+  shell.cp("-R", "src/public/", "build/public/");
+}
