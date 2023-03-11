6146bbe014417048ff427e8643d551c76ed18f9e
Srabo93 <a.s-dev@web.de>
2023-03-11 10:27:07 +0100

    fixed callback url for auth

--- src/config/passport.ts
+++ src/config/passport.ts
@@ -8,7 +8,7 @@ export const passportConfig = passport.use(
     {
       clientID: GOOGLE_CLIENT_ID,
       clientSecret: GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:5000/auth/google/callback",
+      callbackURL: "https://storytime-y7qk.onrender.com/auth/google/callback",
     },
     async (
       accessToken: any,
